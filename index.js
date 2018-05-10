c988bc465333e2213abc0e8a8534e3e5dcc37011
pabloleonalcaide <pabloleonpsico@gmail.co>
2018-05-10 22:02:53 +0200

    test express as middleware

--- index.js
+++ index.js
@@ -5,20 +5,11 @@ const bodyParser = require("body-parser");
 
 const restService = express();
 
-
-/*// Listen for any kind of message. There are different kinds of
-// messages.
-bot.on(/[h]+/, (msg) => {
-  const chatId = msg.chat.id;
-
-  // send a message to the chat acknowledging receipt of their message
-  bot.sendMessage(chatId, 'Received your message');
-});
 restService.use(
   bodyParser.urlencoded({
     extended: true
   })
-);*/
+);
 
 restService.use(bodyParser.json());
 
@@ -46,26 +37,6 @@ let response = function(tipo){
   return response;
 }
 
-const TelegramBot = require('node-telegram-bot-api');
-
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '594433471:AAEa2tidUbvEAau5vUKMjGTIcnUQ_dkqfQs';
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
 
 
 restService.listen(process.env.PORT || 8000, function() {
