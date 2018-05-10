783cc2c715a6b235ab26e96900b22c248093c651
pabloleonalcaide <pabloleonpsico@gmail.co>
2018-05-10 21:47:30 +0200

    test express as middleware

--- index.js
+++ index.js
@@ -4,7 +4,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const restService = express();
+const TelegramBot = require('node-telegram-bot-api');
 
+// replace the value below with the Telegram token you receive from @BotFather
+const token = '594433471:AAEa2tidUbvEAau5vUKMjGTIcnUQ_dkqfQs';
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+bot.setWebHook(`https://grancapibot.herokuapp.com//bot`+token);
+app.use(bot.webhookCallback((`/bot`+token));
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+  // 'msg' is the received Message from Telegram
+  // 'match' is the result of executing the regexp above on the text content
+  // of the message
+
+  const chatId = msg.chat.id;
+  const resp = match[1]; // the captured "whatever"
+
+  // send back the matched "whatever" to the chat
+  bot.sendMessage(chatId, resp);
+});
 
 /*// Listen for any kind of message. There are different kinds of
 // messages.
@@ -46,26 +66,7 @@ let response = function(tipo){
   return response;
 }
 
-const TelegramBot = require('node-telegram-bot-api');
 
-// replace the value below with the Telegram token you receive from @BotFather
-const token = '594433471:AAEa2tidUbvEAau5vUKMjGTIcnUQ_dkqfQs';
-
-// Create a bot that uses 'polling' to fetch new updates
-const bot = new TelegramBot(token, {polling: true});
-
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  // 'msg' is the received Message from Telegram
-  // 'match' is the result of executing the regexp above on the text content
-  // of the message
-
-  const chatId = msg.chat.id;
-  const resp = match[1]; // the captured "whatever"
-
-  // send back the matched "whatever" to the chat
-  bot.sendMessage(chatId, resp);
-});
 
 
 restService.listen(process.env.PORT || 8000, function() {
