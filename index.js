b9e7085c1a04a27b10dc81ea08b31065a5fec762
nievesbb <nievesborrero@gmail.com>
2018-05-11 20:33:27 +0200

    multiple request

--- index.js
+++ index.js
@@ -14,12 +14,23 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/webhook", function(req, res) {
+  /*
   var speech =
     req.body.result &&
     req.body.result.parameters &&
     req.body.result.parameters.tipo
       ? response(req.body.result.parameters.tipo.toLowerCase())
-      : "Ups... ha habido algún problema con nuestra comunicación, sorry!";
+      : "Ups... ha habido algún problema con nuestra comunicación, sorry!";*/
+  var speech;
+  if(req.body.result && req.body.result.parameters){
+    if(req.body.result.parameters.tipo)
+      speech = response(req.body.result.parameters.tipo.toLowerCase());
+    else if(req.body.result.parameters.si-no)
+      speech = 'oki';
+  }else{
+      speech = "Ups... ha habido algún problema con nuestra comunicación, sorry!";
+  }
+
   return res.json({
     speech: speech,
     displayText: speech,
@@ -31,7 +42,7 @@ let response = function(tipo){
   let response;
   switch(tipo){
     case 'daw':  response = 'Aquí hay un montón de información --> http://moodle.iesgrancapitan.org/course/index.php?categoryid=7'; break;
-    case 'asir':  response = 'http://moodle.iesgrancapitan.org/course/index.php?categoryid=4'; break;
+    case 'asir':  response = 'Mira aquí --> http://moodle.iesgrancapitan.org/course/index.php?categoryid=4'; break;
     default: response = 'bollos y magdalenas'; break;
   }
   return response;
