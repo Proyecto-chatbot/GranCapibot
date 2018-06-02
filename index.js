65bef31fc3e3658f3a1567f4a811b22f9e79c513
pabloleonalcaide <pabloleonpsico@gmail.co>
2018-06-02 12:35:23 +0200

    testing integration with giphy api

--- index.js
+++ index.js
@@ -24,51 +24,16 @@ restService.post("/prueba",function(req,res){
   promise.then(function(response){
     console.log(response);
     return res.json({
-      "speech": "this text is spoken out loud if the platform supports voice interactions",
-      "displayText": "this text is displayed visually",
-      "messages": {
-        "type": 1,
-        "title": "card title",
-        "subtitle": "card text",
-        "imageUrl": "https://assistant.google.com/static/images/molecule/Molecule-Formation-stop.png"
-      },
-      "data": {
-        "google": {
-          "expectUserResponse": true,
-          "richResponse": {
-            "items": [
-              {
-                "simpleResponse": {
-                  "textToSpeech": "this is a simple response"
-                }
-              }
-            ]
-          }
-        },
-        "facebook": {
-          "text": "Hello, Facebook!"
-        },
-        "slack": {
-          "text": "This is a text response for Slack."
-        }
-      },
-      "contextOut": [
+      speech: "yo también!",
+      displayText : "yo también!",
+      messages:[
         {
-          "name": "context name",
-          "lifespan": 5,
-          "parameters": {
-            "param": "param value"
-          }
+          type : 3,
+          imageUrl : response
         }
       ],
-      "source": "example.com",
-      "followupEvent": {
-        "name": "event name",
-        "parameters": {
-          "param": "param value"
-        }
-    }
-    
+      source : "webhook-echo-sample"
+      
     },
     
   );
